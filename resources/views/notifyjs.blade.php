d9afcebc6985c37ee72dd78279f722df523ede99
Giang Thai Cuong <thaicuong.giang@gmail.com>
2017-11-13 10:47:20 +0700

    remove notifyjs support

--- resources/views/notifyjs.blade.php
+++
@@ -1,5 +0,0 @@
-@if (session()->has('flash_message'))
-<script type="text/javascript">
-$.Notification.notify("{{ session('flash_message.level') }}", "top right", "{{ session('flash_message.title') }}", "{!! session('flash_message.message') !!}");
-</script>
-@endif
